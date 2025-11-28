01d2760b190642f82d0bcd54b8dffc99815b2e37
David Roze <droze@cirrustech.com.au>
2025-11-28 13:58:29 +1100

    Minor change to FooterComponent.jsx

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const FooterComponent = () => {
   return (
     <div>
-        <footer className='footer'><span>All Rights Reserved 2025 DMR</span></footer>
+        <footer className='footer'><span>All Rights Reserved 2025 David Mark Roze</span></footer>
     </div>
   )
 }
